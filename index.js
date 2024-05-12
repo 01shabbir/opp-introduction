4555d76eae47d2743e0483ad5e9c43ebf9f69cb6
01shabbir <shafiqraza.qureshi01@gmail.com>
2024-05-12 08:50:07 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+class student {
+    name;
+    constructor(n) {
+        this.name = n;
+    }
+}
+class person {
+    students = [];
+    addstudent(obj) {
+        this.students.push(obj);
+    }
+}
+const persons = new person();
+console.log(persons);
+const programstart = async (persons) => {
+    do {
+        console.log("welcome");
+        const ans = await inquirer.prompt({
+            type: "list",
+            message: "ap kis se bat krna chahen ge",
+            name: "select",
+            choices: ["kud se:self", "students"]
+        });
+        if (ans.select == "kud se:self") {
+            console.log("me typescript sekh raha hon");
+            console.log("welcome shabbir Raza");
+            console.log("Allah ne chaha tw kamyabi zaror mele gi");
+        }
+        if (ans.select == "students") {
+            const ans = await inquirer.prompt({
+                type: "input",
+                message: "ap kis se bat krna chahn ge",
+                name: "student",
+            });
+            const student1 = persons.students.find((val) => val.name == ans.student);
+            if (!student1) {
+                const name = new student(ans.student);
+                persons.addstudent(name);
+                console.log(`hello i am ${name.name},or mein thik hon`);
+                console.log(persons.students);
+            }
+            if (student1) {
+                console.log(`hello i am ${student.name}, or mein thik hon......`);
+                console.log(persons.students);
+            }
+        }
+    } while (true);
+};
+programstart(persons);
